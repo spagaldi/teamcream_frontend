0d8907fe46839f0923df4903a85fc92bb5d1f293
Cody Maheu <cody.maheu@navy.mil>
2020-11-12 15:01:16 -0800

    reformat

--- src/components/example.js
+++ src/components/example.js
@@ -2,15 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, Image, View, TextInput, TouchableOpacity, Alert } from 'react-native';
 
-
 const example = () => {
-    return (
-        <View style={{paddingTop: 30}}>
-                <Image source={require('./images/cooking_with_crumbs_logo.PNG')} //TODO: replace with image that's not cut off
-                style={{ width: 275, height: 275 }}/>
-            </View>
-  
-    );
-}
+  return (
+    <View style={{ paddingTop: 30 }}>
+      <Image
+        source={require('../../images/cooking_with_crumbs_logo.PNG')} // TODO: replace with image that's not cut off
+        style={{ width: 275, height: 275 }}
+      />
+    </View>
+  );
+};
 
 export default example;
